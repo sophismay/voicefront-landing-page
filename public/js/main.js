d3d14ce06dd2773a2f80bd044821b3ded06b3604
Masaud <sophismay@gmail.com>
2017-04-21 18:14:15 +0200

    with dynamic images loading

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
 $(function() {
-    //Place your jquery code here.
+    //dynamically add x image
+    displayNegative();
 });
+
+function displayNegative() {
+    setTimeout(function(){
+        $("<img src='images/negative-x.png' id='negative-img'>").css({
+            "position": "absolute", "top": "200px", "right": "0"
+        }).appendTo(".image-container").addClass("animated fadeIn");
+        displayPhone();
+    }, 1000); 
+}
+function displayPhone() {
+    setTimeout( function(){
+        $("#mic-image").addClass("fadeOut").css({"display": "none"});
+        $("#negative-img").addClass("fadeOut").css({"display": "none"});
+        $("<img src='images/app-screen-showcase.png'>").appendTo(".image-container").addClass("animated fadeInRight");
+    }, 900);
+}
